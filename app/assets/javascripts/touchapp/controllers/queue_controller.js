cb59475d1f32fe9c2e99362ec65302374de25f74
Simon Kühling <mail@simonkuehling.de>
2014-08-29 00:18:55 +0200

    rearrange some controls from manual control to new 'print' screen (formerly 'queue')

--- app/assets/javascripts/touchapp/controllers/queue_controller.js
+++ app/assets/javascripts/touchapp/controllers/queue_controller.js
@@ -1,7 +1,15 @@
-touchApp.controller('QueueController', function($scope, MyWebsocket, CommonCode){
+touchApp.controller('QueueController', function($scope, $modal, MyWebsocket, CommonCode){
   console.log("Running QueueController");  
 
-  MyWebsocket.menuDisabled = false;  
+  MyWebsocket.menuDisabled = false; 
+
+ $scope.$watch(function(){ return MyWebsocket.print; }, function(newValue){
+    if (( MyWebsocket.print.state == 2) || ( MyWebsocket.print.state == 5 )) {
+      $scope.printing = true;
+    } else {
+      $scope.printing = false;      
+    }    
+  }, true);    
   
   // init pagination
   $scope.itemsPerPage = 3;
@@ -23,5 +31,19 @@ touchApp.controller('QueueController', function($scope, MyWebsocket, CommonCode)
     $scope.numPrintjobs = MyWebsocket.printjobs.length;    
     $scope.pagedPrintjobs = CommonCode.groupToPages(MyWebsocket.printjobs, $scope.itemsPerPage);
   },true);  
+
+
+  $scope.macro = function( name ){
+    MyWebsocket.macro(name);    
+  };   
+  
+  $scope.psu_off = function(state){
+    // open cooldown dialog
+    var cooldownModal = $modal.open({
+      templateUrl: '<%= asset_path("touchapp/templates/dialog_cooldown.html") %>', // statically included in backendapp.html.erb
+      controller: 'DialogCooldownController',
+      backdrop: 'static'
+    });       
+  };
      
 });
