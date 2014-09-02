86eecfde233d7d48f96912c36651ab575ad6845f
Simon Kühling <mail@simonkuehling.de>
2014-09-02 14:19:58 +0200

    add name of printjob to print_finished screen

--- app/assets/javascripts/touchapp/controllers/print_finished_controller.js
+++ app/assets/javascripts/touchapp/controllers/print_finished_controller.js
@@ -1,11 +1,15 @@
-touchApp.controller('PrintFinishedController', function($scope, $location, $routeParams, MyWebsocket){
+touchApp.controller('PrintFinishedController', function($scope, $location, $routeParams, MyWebsocket, CommonCode){
   console.log("Running PrintFinishedController");
   
   $scope.$watch(function(){ return MyWebsocket.print; }, function(){
     $scope.elapsed = MyWebsocket.print.elapsed_in_words;
   },true);   
   $scope.printjobId = $routeParams.printjobId;
-  
+  $scope.$watch(function(){ return MyWebsocket.printjobs; }, function(){
+    $scope.printjob_name = CommonCode.getById(MyWebsocket.printjobs, $scope.printjobId).name;
+  },true);    
+
+
   $scope.delete = function() {
 console.log("removing printjob ID " + $scope.printjobId);
     MyWebsocket.removePrintjob($scope.printjobId);
