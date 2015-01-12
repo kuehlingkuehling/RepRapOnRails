0cb3261d1c4be580b3579e52ad14f8b52125cd40
Simon Kühling <mail@simonkuehling.de>
2015-01-12 15:31:24 +0100

    fix machine config set for hardware revision v1.0.0

--- config/initializers/repraponrails_02_config.rb
+++ config/initializers/repraponrails_02_config.rb
@@ -14,16 +14,16 @@ RepRapOnRails::Application.configure do
 	  config.chamber_heater_cooldown_time = 180
 	    
 	  # print bed x/y zero position
-	  config.print_bed_zero  = [ 40, 123 ]
+	  config.print_bed_zero  = [ 0, 0 ]
 
 	  # print bed leveling coordinates
-	  config.leveling_point_front  = [ 140, 150 ] 
-	  config.leveling_point_right  = [ 192, 238 ]  
-	  config.leveling_point_left   = [  86, 238 ]  
-	  config.leveling_point_center = [ 112, 213 ]
+    config.leveling_point_front  = [ 100,  36 ] 
+    config.leveling_point_right  = [ 147, 117 ]  
+    config.leveling_point_left   = [  53, 117 ]  
+    config.leveling_point_center = [  71,  90 ]
 
 	  # print head maintenance position
-	  config.maintenance_position = [ 112, 90 ] 
+	  config.maintenance_position = [ 71, -30 ] 
 
   when "v1.1.0"
 
