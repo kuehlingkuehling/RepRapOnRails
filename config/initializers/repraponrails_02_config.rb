0e98f5ca1db20d0a8e7343c0b7dcfd2b52c5dcf2
Simon Kühling <mail@simonkuehling.de>
2015-12-04 14:53:56 +0100

    add new configuration set for hardware revision 1.3.2

--- config/initializers/repraponrails_02_config.rb
+++ config/initializers/repraponrails_02_config.rb
@@ -158,6 +158,32 @@ RepRapOnRails::Application.configure do
     # print head maintenance position
     config.maintenance_position = [ 150, -30 ]    
 
+when "v1.3.2"
+
+      # hardware revision
+    config.hardware_revision = "RepRap Industrial v1.3.2"
+
+    # compatible Arduino Firmware version
+    config.arduino_firmware_version = "Repetier Firmware v0.91-rri1.3.2-01"
+
+    # is dual extruder?
+    config.is_dual_extruder = true    
+
+    # chamber heater cooldown time (for safety) - in seconds
+    config.chamber_heater_cooldown_time = 10
+      
+    # print bed x/y zero position
+    config.print_bed_zero  = [ 0, 0 ]
+
+    # print bed leveling coordinates
+    config.leveling_point_front  = [ 100,  36 ] 
+    config.leveling_point_right  = [ 147, 117 ]  
+    config.leveling_point_left   = [  53, 117 ]  
+    config.leveling_point_center = [  71,  90 ]
+
+    # print head maintenance position
+    config.maintenance_position = [ -29, -30 ]
+  
   end
   	
 end
