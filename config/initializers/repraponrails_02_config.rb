2d23992751c75b8cbbcf31b7d3c67bf7eba320f4
Simon Kühling <mail@simonkuehling.de>
2016-01-13 12:35:26 +0100

    add configuration for hardware revision 1.4.0-S300200

--- config/initializers/repraponrails_02_config.rb
+++ config/initializers/repraponrails_02_config.rb
@@ -209,7 +209,33 @@ RepRapOnRails::Application.configure do
 
     # print head maintenance position
     config.maintenance_position = [ -29, -30 ]    
-  
+
+  when "v1.4.0-S300200"
+
+    # hardware revision
+    config.hardware_revision = "HT500 v1.4.0-S300200"
+
+    # compatible Arduino Firmware version
+    config.arduino_firmware_version = "Repetier Firmware v0.91-rri1.4.0-S300200-01"
+
+    # is dual extruder?
+    config.is_dual_extruder = false
+
+    # chamber heater cooldown time (for safety) - in seconds
+    config.chamber_heater_cooldown_time = 10
+      
+    # print bed x/y zero position
+    config.print_bed_zero  = [ 0, 0 ]
+
+    # print bed leveling coordinates
+    config.leveling_point_front  = [ 150,  36 ] 
+    config.leveling_point_right  = [ 197, 117 ]  
+    config.leveling_point_left   = [ 103, 117 ]  
+    config.leveling_point_center = [ 121,  90 ]
+
+    # print head maintenance position
+    config.maintenance_position = [ 150, -30 ]  
+
   end
   	
 end
