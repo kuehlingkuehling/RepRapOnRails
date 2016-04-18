f7a5c7dd9da34fa4f0da4eecba023a39598713f3
Simon Kühling <mail@simonkuehling.de>
2016-04-18 13:32:16 +0200

    prevent right-click context menu to open with long tap in chromium browser (touchapp)

---
+++ app/assets/javascripts/touchapp/directives/prevent-right-click.js
@@ -0,0 +1,13 @@
+touchApp.directive('preventRightClick', [
+
+            function() {
+                return {
+                    restrict: 'A',
+                    link: function($scope, $ele) {
+                        $ele.bind("contextmenu", function(e) {
+                            e.preventDefault();
+                        });
+                    }
+                };
+            }
+        ])
