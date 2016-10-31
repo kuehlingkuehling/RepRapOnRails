2e829d6b865f5a86c332edeac00370a4d5f3321c
Simon Kühling <mail@simonkuehling.de>
2016-10-31 23:36:16 +0100

    add RODIN4D branding scheme

--- config/initializers/repraponrails_02_config.rb
+++ config/initializers/repraponrails_02_config.rb
@@ -2,135 +2,64 @@ RepRapOnRails::Application.configure do
   # Configuring all the hardware revision specific parameters
 
   case config.hardware_revision_number
-  when "v1.4.1"
+  
+  when "vp75-op-1.0.0"
 
-      # hardware revision
-    config.hardware_revision = "HT500 v1.4.1"
+    # model
+    config.model_name = "VP75 OP"
+
+    # hardware revision
+    config.hardware_revision = "1.0.0"
+
+    # customized interface design
+    config.header_background_image = "rodin4d-background.jpg"
+    #config.brand_color = "#7FFF00"
+    config.brand_color = "#d9534f"
 
     # compatible Arduino Firmware version
-    config.arduino_firmware_version = "Repetier Firmware v0.92-ht500-1.4.1-01"
+    config.arduino_firmware_version = "Repetier Firmware v0.92-vp75-1.0.0-01"
 
     # is dual extruder?
-    config.is_dual_extruder = true
+    config.is_dual_extruder = false
 
     # chamber heater cooldown time (for safety) - in seconds
-    config.chamber_heater_cooldown_time = 10
+    config.chamber_heater_cooldown_time = 120
 
     # print bed x/y zero position
     config.print_bed_zero  = [ 0, 0 ]
 
-    # print bed leveling coordinates
-    config.leveling_point_front  = [ 100,  36 ]
-    config.leveling_point_right  = [ 147, 117 ]
-    config.leveling_point_left   = [  53, 117 ]
-    config.leveling_point_center = [  71,  90 ]
-
     # print head maintenance position
-    config.maintenance_position = [ -29, -30 ]    
+    config.maintenance_position = [ 0, -200 ]
 
-  when "v1.4.2"
+  when "vp75-1.0.0"
 
-      # hardware revision
-    config.hardware_revision = "HT500 v1.4.2"
+    # model
+    config.model_name = "VP75"
+
+    # hardware revision
+    config.hardware_revision = "1.0.0"
+
+    # customized interface design
+    config.header_background_image = "blurry-autumn-background.jpg"
+    config.brand_color = "#7FFF00"
 
     # compatible Arduino Firmware version
-    config.arduino_firmware_version = "Repetier Firmware v0.92-ht500-1.4.2-01"
+    config.arduino_firmware_version = "Repetier Firmware v0.92-vp75-1.0.0-01"
 
     # is dual extruder?
-    config.is_dual_extruder = true
+    config.is_dual_extruder = false
 
     # chamber heater cooldown time (for safety) - in seconds
-    config.chamber_heater_cooldown_time = 10
+    config.chamber_heater_cooldown_time = 120
 
     # print bed x/y zero position
     config.print_bed_zero  = [ 0, 0 ]
 
-    # print bed leveling coordinates
-    config.leveling_point_front  = [ 100,  36 ]
-    config.leveling_point_right  = [ 147, 117 ]
-    config.leveling_point_left   = [  53, 117 ]
-    config.leveling_point_center = [  71,  90 ]
-
     # print head maintenance position
-    config.maintenance_position = [ -29, -30 ]    
-    
-  when "v1.4.3"
+    config.maintenance_position = [ 0, -200 ]
 
-      # hardware revision
-    config.hardware_revision = "HT500 v1.4.3"
-
-    # compatible Arduino Firmware version
-    config.arduino_firmware_version = "Repetier Firmware v0.92-ht500-1.4.3-02"
-
-    # is dual extruder?
-    config.is_dual_extruder = true
-
-    # chamber heater cooldown time (for safety) - in seconds
-    config.chamber_heater_cooldown_time = 10
-
-    # print bed x/y zero position
-    config.print_bed_zero  = [ 0, 0 ]
-
-    # print bed leveling coordinates
-    config.leveling_point_front  = [ 100,  36 ]
-    config.leveling_point_right  = [ 147, 117 ]
-    config.leveling_point_left   = [  53, 117 ]
-    config.leveling_point_center = [  71,  90 ]
-
-    # print head maintenance position
-    config.maintenance_position = [ -29, -30 ]
-
-  when "v1.4.4"
-
-      # hardware revision
-    config.hardware_revision = "HT500 v1.4.4"
-
-    # compatible Arduino Firmware version
-    config.arduino_firmware_version = "Repetier Firmware v0.92-ht500-1.4.4-01"
-
-    # is dual extruder?
-    config.is_dual_extruder = true
-
-    # chamber heater cooldown time (for safety) - in seconds
-    config.chamber_heater_cooldown_time = 10
-
-    # print bed x/y zero position
-    config.print_bed_zero  = [ 0, 0 ]
-
-    # print bed leveling coordinates
-    config.leveling_point_front  = [ 100,  36 ]
-    config.leveling_point_right  = [ 147, 117 ]
-    config.leveling_point_left   = [  53, 117 ]
-    config.leveling_point_center = [  71,  90 ]
-
-    # print head maintenance position
-    config.maintenance_position = [ -29, -30 ]    
-
-  when "v1.4.5"
-
-      # hardware revision
-    config.hardware_revision = "HT500 v1.4.5"
-
-    # compatible Arduino Firmware version
-    config.arduino_firmware_version = "Repetier Firmware v0.92-ht500-1.4.5-01"
-
-    # is dual extruder?
-    config.is_dual_extruder = true
-
-    # chamber heater cooldown time (for safety) - in seconds
-    config.chamber_heater_cooldown_time = 10
-
-    # print bed x/y zero position
-    config.print_bed_zero  = [ 0, 0 ]
-
-    # print bed leveling coordinates
-    config.leveling_point_front  = [ 100,  36 ]
-    config.leveling_point_right  = [ 147, 117 ]
-    config.leveling_point_left   = [  53, 117 ]
-    config.leveling_point_center = [  71,  90 ]
-
-    # print head maintenance position
-    config.maintenance_position = [ -29, -30 ]
   end
+
+
   	
 end
