c2e63cbdb38b3f098ad97665bca814a8368e2f2c
Simon Kühling <mail@simonkuehling.de>
2017-05-09 12:32:55 +0200

    add timestamp to log filename, sort by id instead created_at, add log file entry id to each line

--- app/controllers/backendapp_controller.rb
+++ app/controllers/backendapp_controller.rb
@@ -11,12 +11,13 @@ class BackendappController < ApplicationController
   end
   
   def logfile
-    @lines = LogEntry.order("created_at DESC").limit(20000)
+    @lines = LogEntry.order("id DESC").limit(20000)
     logfile = ""
     @lines.each do |l|
-      logfile += "[#{l.created_at.utc.to_s}] (#{l.level}) #{l.line}\n"
+      logfile += "#{l.id}\t[#{l.created_at.utc.to_s}]\t(#{l.level})\t#{l.line}\n"
     end    
-    send_data logfile, :filename => "RepRapIndustrial_logfile.txt"
+    timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
+    send_data logfile, :filename => "RepRapOnRails_#{timestamp}.log"
   end
   
   def upload
