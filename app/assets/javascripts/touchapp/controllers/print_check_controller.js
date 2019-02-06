ac494c87bda09a0a66c0a0e8fa9d200ad219fc2d
Simon Kühling <mail@simonkuehling.de>
2019-02-06 16:05:30 +0100

    display job name and note for reference in safety-check screen prior to printing

--- app/assets/javascripts/touchapp/controllers/print_check_controller.js
+++ app/assets/javascripts/touchapp/controllers/print_check_controller.js
@@ -1,6 +1,11 @@
-touchApp.controller('PrintCheckController', function($scope, $location, $routeParams, Printer){
+touchApp.controller('PrintCheckController', function($scope, $location, $routeParams, Printer, CommonCode){
   console.log("Running PrintCheckController");
   
+  $scope.printjobId = $routeParams.printjobId;
+  $scope.$watch(function(){ return Printer.printjobs; }, function(){
+    $scope.printjob = CommonCode.getById(Printer.printjobs, $scope.printjobId);
+  },true);   
+
   $scope.startprint = function() {
     Printer.startprint($routeParams.printjobId);
     $location.path( "/queue" );
