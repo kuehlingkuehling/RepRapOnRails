7f2ffefa109ce7fe8ad1a8a94bab78268c1ca947
Simon Kühling <mail@simonkuehling.de>
2020-08-13 13:56:30 +0200

    add configuration set for hardware revision 1.2.1

--- config/initializers/repraponrails_01_config.rb
+++ config/initializers/repraponrails_01_config.rb
@@ -169,6 +169,33 @@ RepRapOnRails::Application.configure do
     # print head maintenance position
     config.maintenance_position = [ 0, -150, 600 ]
 
+  when "vp75-1.2.1"
+
+    # model
+    config.model_name = "VP75"
+
+    # hardware revision
+    config.hardware_revision = "1.2.1"
+
+    # customized interface design
+    config.header_background_image = "blurry-autumn-background.jpg"
+    config.brand_color = "#7FFF00"
+
+    # compatible Arduino Firmware version
+    config.arduino_firmware_version = "Repetier Firmware v0.92-vp75-1.2.1-01"
+
+    # is dual extruder?
+    config.is_dual_extruder = false
+
+    # chamber heater cooldown time (for safety) - in seconds
+    config.chamber_heater_cooldown_time = 120
+
+    # print bed x/y zero position
+    config.print_bed_zero  = [ 0, 0 ]
+
+    # print head maintenance position
+    config.maintenance_position = [ 0, -150, 600 ]
+
   end
 
 
